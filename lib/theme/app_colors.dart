49d2a7ec5c812319a0edab6370f61e8d4ef93855
Akbar2612 <akbarfirdaus433@gmail.com>
2025-10-17 23:56:45 +0700

    update

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Color(0xFF1E1E1E); // abu tua hampir hitam
-  static const Color accent = Color(0xFF3C3C3C); // abu sedang
-  static const Color background = Color(0xFF121212); // hitam untuk latar belakang
-  static const Color surface = Color(0xFF1F1F1F); // untuk card / container
-  static const Color textPrimary = Colors.white;
+  static const Color primary = Color.fromARGB(255, 23, 23, 23);
+  static const Color accent = Color.fromARGB(255, 92, 92, 92);
+  static const Color background = Color(0xFFFFFAF0);
+  static const Color textPrimary = Colors.black87;
   static const Color textSecondary = Colors.grey;
-  static const Color divider = Color(0xFF2C2C2C);
+  static const Color cardBackground = Colors.white;
 }
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,50 +1,32 @@
 import 'package:flutter/material.dart';
-import 'app_colors.dart';
 
 class AppTheme {
   static ThemeData get lightTheme {
     return ThemeData(
       useMaterial3: true,
-      brightness: Brightness.dark,
-      colorScheme: const ColorScheme.dark(
-        primary: AppColors.primary,
-        secondary: AppColors.accent,
-        background: AppColors.background,
-        surface: AppColors.surface,
-      ),
-      scaffoldBackgroundColor: AppColors.background,
+      colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF8E6CEF)),
+      scaffoldBackgroundColor: Colors.white,
       fontFamily: 'Poppins',
-
       textTheme: const TextTheme(
         headlineLarge: TextStyle(
           fontSize: 26,
           fontWeight: FontWeight.bold,
-          color: AppColors.textPrimary,
+          color: Colors.black,
         ),
         titleMedium: TextStyle(
           fontSize: 18,
           fontWeight: FontWeight.w600,
-          color: AppColors.textPrimary,
+          color: Colors.black87,
         ),
         bodyMedium: TextStyle(
           fontSize: 14,
-          color: AppColors.textSecondary,
+          color: Colors.black87,
         ),
       ),
-
       appBarTheme: const AppBarTheme(
-        backgroundColor: AppColors.surface,
-        foregroundColor: AppColors.textPrimary,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
         elevation: 0,
-        centerTitle: true,
-      ),
-
-      cardColor: AppColors.surface,
-      dividerColor: AppColors.divider,
-      iconTheme: const IconThemeData(color: AppColors.textPrimary),
-      floatingActionButtonTheme: const FloatingActionButtonThemeData(
-        backgroundColor: AppColors.accent,
-        foregroundColor: AppColors.textPrimary,
       ),
     );
   }
