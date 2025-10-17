cb45ea08611a05eee3244c28b137cd7ac1fb3368
Akbar2612 <akbarfirdaus433@gmail.com>
2025-10-17 23:39:03 +0700

    update modulasi tema

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFFFFB6C1);
+  static const Color accent = Color(0xFFFF69B4);
+  static const Color background = Color(0xFFFFFAF0);
+  static const Color textPrimary = Colors.black87;
+  static const Color textSecondary = Colors.grey;
+  static const Color cardBackground = Colors.white;
+}
