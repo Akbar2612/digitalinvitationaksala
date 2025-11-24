49ad3513a8af520b5fee7ae915f489349466cbd4
Akbar2612 <akbarfirdaus433@gmail.com>
2025-11-24 15:31:43 +0700

    update web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -57,7 +57,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ed9ea00ebcdc8ea3617be7f8e2eadf38
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "1fc190034bda2520662071b4fa2ce37c",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "bf7b4d24173e25a77f045917a39278d0",
+"flutter_bootstrap.js": "647a66c7ce42355e2c51b3278860ebf7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
