266e0231adc6587390c12ffa09b83e33fe609dcf
Akbar2612 <akbarfirdaus433@gmail.com>
2025-11-28 19:35:32 +0700

    update

--- lib/shared/widget_playlist_data.dart
+++ lib/shared/widget_playlist_data.dart
@@ -7,28 +7,4 @@ final List<PlaylistItem> weddingPlaylist = [
     path: 'music/musicbg.mp3',
     duration: '4:39',
   ),
-  PlaylistItem(
-    title: 'Gunslinger',
-    artist: 'Avenged Sevenfold',
-    path: 'music/gunslinger.mp3',
-    duration: '4:12',
-  ),
-  PlaylistItem(
-    title: 'Akad',
-    artist: 'Payung Teduh',
-    path: 'music/akad.mp3',
-    duration: '4:16',
-  ),
-  PlaylistItem(
-    title: 'Lover',
-    artist: 'Taylor Swift',
-    path: 'music/lover.mp3',
-    duration: '3:41',
-  ),
-  PlaylistItem(
-    title: 'Janji Suci',
-    artist: 'Yovie & Nuno',
-    path: 'music/janjisuci.mp3',
-    duration: '3:41',
-  ),
 ];
